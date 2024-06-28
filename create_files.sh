411ab038d324213d79ac2cf6bca001bda8ebbb62
Thejan Vithanage <thejanvithanage@outlook.com>
2024-06-28 20:03:32 +0530

    Add script create-files

---
+++ create_files.sh
@@ -0,0 +1,14 @@
+file_01="Japan"
+file_02="Egypt"
+file_O3="Canada"
+
+cd /home/bob
+
+echo "Creating file called $file_01"
+touch $file_01
+
+echo "Creating file called $file_02"
+touch $file_02
+
+echo "Creating file called $file_03"
+touch $file_03
